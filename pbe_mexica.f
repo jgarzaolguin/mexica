e02814a007695703f9354ffe71ff116348d8d582
jgarzaolguin <jgarzaolguin@gmail.com>
2018-05-17 12:31:42 -0500

    xc pbe final 1

--- pbe_mexica.f
+++ pbe_mexica.f
@@ -132,11 +132,11 @@ c*********************************************
 c Derivada funcional para ser escrita sobre un grid
 c Solamente para \'atomos
 c************************************************
-      subroutine pbegrid(rho, derho, secdrho, varS, der_varS, grid,
-     &                   pot_x)
+      subroutine pbegrid(rho, derho, secdrho, normdrho, dernormrho,
+     &           varS, der_varS, grid, pot_x)
       implicit none
       double precision rho, varS, der_varS, pot_x, derho, secdrho,
-     &                 grid, laplac
+     &                 grid, laplac, normdrho, dernormrho
       double precision term1, term2, kappa, mu, cuadS, frac, pi
       double precision eps_x, Fx, der_Fx
       pi = 4d00*datan(1d00)
@@ -149,14 +149,23 @@ c************************************************
       endif
       term1 = term1*derho
       laplac = secdrho + term1
+      if (normdrho.gt.0d00) then
+        term2 = derho*dernormrho/normdrho
+        term2 = (laplac - term2)*der_Fx(varS)/normdrho
+      else
+        term2 = 0d00
+      endif
+      pot_x = term2
       term1 = 4d00*eps_x(rho)*(Fx(varS) - varS*der_Fx(varS))/3d00
       cuadS = varS*varS
       frac = mu/kappa
       term2 = kappa - 3d00*mu*cuadS
       term2 = term2/((1d00 + frac*cuadS)**3d00)
-      term2 = 3d00*frac*term2/(4d00*pi)
+      term2 = 2d00*frac*term2
       term2 = term2*der_varS
-      pot_x = term1 + term2
+      pot_x = pot_x + term2
+      pot_x = -3d00*pot_x/(8d00*pi)
+      pot_x = term1 - pot_x
       return
       end
 c****************************************
