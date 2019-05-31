8054b950c1252f3dfa0f7d609b2d97e4da29f9dd
jgarzaolguin <jgarzaolguin@gmail.com>
2019-05-31 09:15:17 -0500

    Changing grid

--- grid.c
+++ grid.c
@@ -13,19 +13,24 @@
 
  int unsigned i;
  double point_r;
- double p1;
- int save_temp;
+ double p1, pi;
+ int save_temp, damping;
 
+ pi = 4.f*atan(1.f);
  save_temp = 0;
 
+ damping = 64;
  grid[0] = 0.f;
 
  for (i = 1; i < n_points; i++) { //for n_points
-    point_r = -5.f + (double) (i - 1)/32;                         // maya tipo Froese Fischer
+    point_r = -5.f + (double) (i - 1)/damping;                         // maya tipo Froese Fischer
     p1 = exp(point_r)/z;               // rj                      // p1 = [e^(point_r)]/z
 
-    if(p1 == Rc && strcmp(bound,"finite") == 0 || strcmp(bound,"dielectricc") == 0 || strcmp(bound,"polarization") == 0){
-      point_r = -5.f + (double) (i)/32;                         // maya tipo Froese Fischer
+    point_r = 1.f - cos(pi*((double) i/(2.f*(n_points+1))));
+    p1 = 200.f*point_r;
+
+    if((p1 == Rc && strcmp(bound,"finite") == 0) || (strcmp(bound,"dielectricc") == 0) || (strcmp(bound,"polarization") == 0)){
+      point_r = -5.f + (double) (i)/damping;                         // maya tipo Froese Fischer
       p1 = exp(point_r)/z;    
      }
       
